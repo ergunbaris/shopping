409f550abda94e7f8cbc16165e51112bd90b4f01
Baris Ergun <baris@pairwiseltd.com>
2021-02-14 23:55:01 +0000

    step 1a

--- src/main/scala/example/ShoppingCart.scala
+++ src/main/scala/example/ShoppingCart.scala
@@ -7,13 +7,13 @@ object ShoppingCart {
     val apple = Item(Symbol("apple"), APPLE_PRICE)
     val orange = Item(Symbol("orange"), ORANGE_PRICE)
 
-    assert(checkout(Seq(apple, orange, apple, orange)) == APPLE_PRICE * 2 + ORANGE_PRICE * 2)
-    assert(checkout((0 until 20).map(_ => apple) ++ (0 until 10).map(_ => orange)) == APPLE_PRICE * 20 + ORANGE_PRICE * 10)
-    
+    assert(checkout(Seq(apple, orange, apple, orange)) == BigDecimal(170)/100)
+    assert(checkout((0 until 20).map(_ => apple) ++ (0 until 10).map(_ => orange)) == BigDecimal(1450)/100)
+
 
   }
 
-  def checkout(items: Seq[Item]): Int = items.map(_.priceInPence).sum
+  def checkout(items: Seq[Item]): BigDecimal = BigDecimal(items.map(_.priceInPence).sum)/100
 }
 
 case class Item(name: Symbol, priceInPence: Int)
